5940437c2aee70fe90b2d758269900b4f0c38023
Your Name <gantonijer@gmail.com>
2018-06-03 20:53:12 +0200

    Http get pipe

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var http=require('http');
+var bl=require('bl');
+
 http.get(process.argv[2],function(response){
-    response.setEncoding('utf8')
-    response.on('data',function(data){
-        console.log(data);
-    })
+   response.pipe(bl(function(err,data){
+       if(err){
+        return console.error(data)
+       }
+            
+        
+        console.log(data.length);
+        console.log(data.toString());
+   }))
 
 })
 
