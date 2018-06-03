26533b3735dd76a22df6f61621b85c8a30fb4bec
Your Name <gantonijer@gmail.com>
2018-06-03 21:09:10 +0200

    Forcing async in order

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var http=require('http');
 var bl=require('bl');
+var rez=[];
+var count=0;
 
-http.get(process.argv[2],function(response){
-   response.pipe(bl(function(err,data){
-       if(err){
-        return console.error(data)
-       }
-            
-        
-        console.log(data.length);
-        console.log(data.toString());
-   }))
+function httpGet(index){
+    http.get(process.argv[2+index],function(response){
+        response.pipe(bl(function(err,data){
+            if(err){
+                return console.error(data);
+            }
+            rez[index]=data.toString();
+            count++
+            if(count===3){
+                for(var i=0;i<3;i++){
+                    console.log(rez[i]);
+                }
+            }
+        }))
+    })
+}
 
-})
-
-
+for(var i=0;i<3;i++){
+    httpGet(i);
+}
+   
