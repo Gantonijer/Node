0ef48dd129ee36e616a2301bdf34d7c6d09afcb1
Your Name <gantonijer@gmail.com>
2018-06-04 13:39:38 +0200

    Http file exercise

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 var http=require('http');
-var bl=require('bl');
-var rez=[];
-var count=0;
+var fs=require('fs');
+var port=process.argv[2];
+var file=process.argv[3];
 
-function httpGet(index){
-    http.get(process.argv[2+index],function(response){
-        response.pipe(bl(function(err,data){
-            if(err){
-                return console.error(data);
-            }
-            rez[index]=data.toString();
-            count++
-            if(count===3){
-                for(var i=0;i<3;i++){
-                    console.log(rez[i]);
-                }
-            }
-        }))
-    })
-}
 
-for(var i=0;i<3;i++){
-    httpGet(i);
-}
+var server = http.createServer(function(req,res){
+    res.writeHead(200,{'content-type':'text-plain'})
+    fs.createReadStream(file).pipe(res);
    
+
+
+
+})
+server.listen(process.argv[2])
